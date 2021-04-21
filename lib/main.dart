bfbcec8a24f73833335fd564cdb7331b1f3612c2
Sourabh <suman.sou62@gmail.com>
2021-04-22 02:33:09 +0530

    buttons added

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,34 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void ts() => print("qwerty");
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Flutter Basics'),
-            ),
-            body: Text('This is body.')));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Basics'),
+          ),
+          body: Column(
+            //invisible widgets
+            children: [
+              //list
+              FloatingActionButton(
+                child: Text('Btn1'),
+                onPressed: ts,
+              ),
+              FloatingActionButton(
+                child: Text('Btn2'),
+                onPressed: ts,
+              ),
+              // ignore: deprecated_member_use
+              RaisedButton(
+                child: Text('Btn3'),
+                onPressed: ts,
+              ),
+            ],
+          )),
+    );
   }
 }
