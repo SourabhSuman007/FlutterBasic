525ee5e5b0bee16f8b16d8012a3a103cd867cae5
Sourabh <suman.sou62@gmail.com>
2021-04-22 11:33:35 +0530

    added question list (static version)

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,52 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  void ts() => print("qwerty");
+  //void ts() => print("qwerty");
+
+  var qIndex = 0;
+  void ansQ() {
+    qIndex = qIndex + 1;
+    print(qIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'Do you like CSGO?',
+      'Do you code?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter Basics'),
-          ),
-          body: Column(
-            //invisible widgets
-            children: [
-              //list
-              FloatingActionButton(
-                child: Text('Btn1'),
-                onPressed: ts,
-              ),
-              FloatingActionButton(
-                child: Text('Btn2'),
-                onPressed: ts,
-              ),
-              // ignore: deprecated_member_use
-              RaisedButton(
-                child: Text('Btn3'),
-                onPressed: ts,
-              ),
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('Flutter Basics'),
+        ),
+        body: Column(
+          //invisible widgets
+          children: [
+            //list
+            Text(questions[qIndex]),
+            FloatingActionButton(
+              child: Text('Btn1'),
+              onPressed: ansQ,
+            ),
+            FloatingActionButton(
+              child: Text('Btn2'),
+              onPressed: () {
+                //dsd
+                print('Btn2 pressed.');
+              },
+            ),
+            // ignore: deprecated_member_use
+            RaisedButton(
+              child: Text('Btn3'),
+              onPressed: () => {
+                print('Btn3 pressed.'),
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
