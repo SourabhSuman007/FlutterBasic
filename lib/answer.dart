70e9babd2450a94aca800c778cf6dd16b45e518e
Sourabh <suman.sou62@gmail.com>
2021-04-23 10:11:17 +0530

    removed callback, implemented Iterables using map method, added quiz completion message

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function stateChanger;
-  Answer(this.stateChanger);
+  final String answer;
+  Answer(this.stateChanger, this.answer);
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ElevatedButton(
-        child: Text('Btn_1'),
+        child: Text(answer),
         onPressed: stateChanger,
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:easy_gradient_text/easy_gradient_text.dart';
 
 import './question.dart';
 import './answer.dart';
@@ -14,6 +15,37 @@ class MyApp extends StatefulWidget {
 
 //void ts() => print("qwerty");
 class _MyAppState extends State<MyApp> {
+  var questions = [
+    {
+      'question': 'Do you play CSGO?',
+      'answer': ['Yes', 'No'],
+    },
+    {
+      'question': 'Do you code?',
+      'answer': ['Yes', 'No'],
+    },
+    {
+      'question': 'What is the color of the sky today?',
+      'answer': ['Blue', 'White', 'Red', 'Purple', 'I\'m blind, can\'t see.'],
+    },
+    {
+      'question': 'What comes first?',
+      'answer': ['Hen', 'Egg'],
+    },
+    {
+      'question': 'How many colors does an Indian flag constitute?',
+      'answer': ['1', '2', '3', 'Nahi Pata!'],
+    },
+    {
+      'question': 'What is a color of white?',
+      'answer': ['Obviously, White.', 'Black', 'Combo of all colors.'],
+    },
+    {
+      'question': 'What is a color of white?',
+      'answer': ['Obviously, Black.', 'White', 'It is not a color!!'],
+    }
+  ];
+
   var _qIndex = 0;
 
   void _ansQ() {
@@ -25,26 +57,40 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'Do you like CSGO?',
-      'Do you code?',
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Basics'),
         ),
-        body: Column(
-          //invisible widgets
-          children: [
-            //list
-            Question(questions[_qIndex]),
-            Answer(_ansQ), //callbacks
-            Answer(_ansQ),
-            Answer(_ansQ),
-          ],
-        ),
+        body: (_qIndex < questions.length)
+            ? Column(
+                //invisible widgets
+                children: [
+                  //list
+                  Question(questions[_qIndex]['question']),
+                  //iterating over answer list in the map.
+                  ...(questions[_qIndex]['answer'] as List<String>)
+                      .map((answers) {
+                    return Answer(_ansQ, answers);
+                  })
+                  // for (var answers in questions[_qIndex]['answer']){
+                  //   print ('$answers');
+                  // }
+                ],
+              )
+            : Center(
+                child: Container(
+                  child: GradientText(
+                    text: 'Quiz Over!!',
+                    colors: [Colors.red[600], Colors.orange[400]],
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    //textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
       ),
     );
   }
